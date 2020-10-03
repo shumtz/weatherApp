8da62235d5189a34621dfb59d2a47d4063b07944
shumtz <shumtztuber2@gmail.com>
2020-10-03 16:59:03 -0300

    first

---
+++ assets/main.js
@@ -0,0 +1,28 @@
+const searchBox = document.getElementById('input')
+const cityName = document.getElementById('city')
+const temp = document.getElementById('temp')
+const maxTemp = document.getElementById('max')
+const minTemp = document.getElementById('min')
+const weather = document.getElementById('weather')
+const erro = document.getElementById('erro')
+
+const URL = 'http://api.openweathermap.org/data/2.5/'
+const KEY = '7d738284923b0d94731dbf571f735771'
+
+searchBox.addEventListener('keypress', setKeyboard)
+
+async function getWeather(query) {
+  const response = await fetch(`${URL}weather?q=${query}&units=metric&appid=${KEY}`)
+  const result = await response.json()
+  cityName.innerHTML = `${result.name}, ${result.sys.country}`
+  temp.innerHTML = `${Math.round(result.main.temp)} °C`
+  maxTemp.innerHTML = `${Math.round(result.main.temp_max)} °C`
+  minTemp.innerHTML = `${Math.round(result.main.temp_min)} °C`
+  weather.innerHTML = `${result.weather[0].main}`
+}
+
+function setKeyboard(e) {
+  if (e.keyCode == 13) {
+    getWeather(searchBox.value)
+  }
+}
